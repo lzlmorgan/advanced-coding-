e7d6ccac2c376f466e9a31fa9a1e3b80dd2056b5
lzlmorgan <morganliu0314@outlook.com>
2022-06-16 13:49:30 -0400

    final version to explain the relation between object, type, class

--- 進階觀念/inheritance.py
+++ 進階觀念/inheritance.py
@@ -8,9 +8,31 @@ class Drink(Product):
 
 d = Drink('Tekila', 100, 600)
 print(d.volume)
-print('--------------------')
 print(type(d)) #instance實例d的class資料類別
 print(isinstance(d, Drink))#判斷d是不是屬於class Drink的類別
 print(isinstance(d, Product)) ##判斷d是不是屬於class Product的類別
-print(type(Drink))
+print(type(Drink)) #class Drink的type是type
+print('-----------------------------------------')
+print(isinstance(Drink, type)) #class drink是type的實例
+print(isinstance(type,object)) #type 是 object的實例
+print(isinstance(Drink, object)) #class drink也是object的實例
+
+
+print('----------------------------------------')
+class Food(Product):
+	pass
+
+Food = type('Food', (Product,), {})
+f = Food('pasta', 220)
+print(type(f))
+
+#以上两个表达了同样的意思 创建class food并继承Produc
+#type类别 创建出 class 创建出 instance
+#class是type的實例instance
+#再創建class的實例instance
+
+print('----------------------------------------')
+
+
+
 
