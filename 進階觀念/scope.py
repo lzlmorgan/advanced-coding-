66477fd623607083a239fc62b2c0443ef953e34d
lzlmorgan <morganliu0314@outlook.com>
2022-06-15 21:20:58 -0400

    5version of adding commence

--- 進階觀念/scope.py
+++ 進階觀念/scope.py
@@ -6,6 +6,15 @@
 #B: built_in 高
 #底层对高层只能读取不能写入
 
+
+
+#顺序
+#权限
+#global語法
+#nonlocal語法
+#lifecycle: def(function)運作了,local區域才工作--生命週期
+#不建議用同名
+
 x = 1
 def f():
 	global x  #global可以跨区域取值
@@ -18,7 +27,7 @@ print('--------------------')
 def f():
 	x = 1
 	def ff():
-		nonlocal x #跨enclosed取local值
+		  nonlocal x #跨enclosed取local值
 		x = 10
 	ff()
 	print(x)
