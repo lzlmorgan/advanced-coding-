64ca36ad0e764f454957a3251535491d57f036b6
lzlmorgan <morganliu0314@outlook.com>
2022-06-16 16:44:14 -0400

    2nd version of adding time-elspsed

--- 進階觀念/decorator.py
+++ 進階觀念/decorator.py
@@ -1,10 +1,20 @@
 #裝飾器 decorator
+import time
 
 def print_func(func):
 	def inner():
 		print('running', func.__name__)
 		func()
-	return inner
+	return inner   #跑func名字
+
+
+def time_func(func):
+	def inner():
+		start = time.time()
+		func()
+		end = time.time()
+		print('elapsed', end - start, 'secs')
+	return inner   #跑時間差
 
 def test():
 	print('hi')
@@ -13,8 +23,14 @@ def test2():
 	print('hello')
 
 
-print('in processing')
+print('--------in processing--------')
 test = print_func(test)
 test2 = print_func(test2)
+test()
+test2()
+
+print('---------in processing--------')
+test = time_func(test)
+test2 = time_func(test2)
 test()
 test2()
