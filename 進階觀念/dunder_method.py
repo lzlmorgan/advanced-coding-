5e3c82b83134bf73a2413138ac1d4b97e77f04be
lzlmorgan <morganliu0314@outlook.com>
2022-06-15 22:43:33 -0400

    4th version of __mul__

--- 進階觀念/dunder_method.py
+++ 進階觀念/dunder_method.py
@@ -20,10 +20,19 @@ class Product:
 		if isinstance(other, Product):
 			return [self, other]
 
+	def __mul__(self, other):
+		re = []
+		if isinstance(other, int): #創建實例instance other是int
+			for i in range(other):
+				re.append(self)
+		return re
+
+
 #p = Product('bublle tea', 60) #創建class類,然後建立class的實例instance
 #p + ' cream'
 #print(p)
 #print(repr(p))
 p1 = Product('ramen', 60)
 p2 = Product('tea', 200)
-print(p1 + p2) #清單+清單=清單
+# print(p1 + p2) #清單+清單=清單
+print(p1*5)
