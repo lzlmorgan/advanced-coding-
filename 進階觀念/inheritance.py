1f6f9aa50fb5afde7acc69f03df520213b3cbc3e
lzlmorgan <morganliu0314@outlook.com>
2022-06-15 23:12:56 -0400

    2ndt

--- 進階觀念/inheritance.py
+++ 進階觀念/inheritance.py
@@ -2,10 +2,9 @@ from dunder_method import Product #parent class/base class
 
 #child class/derived class
 class Drink(Product):
-	def __init__(self, name, price, volume): #child overwrite the parent
-		self.name = name
-		self.price = price
-		self.volume = volume
+	def __init__(self, name, price, volume): 
+		super().__init__(name, price) #supper().__init__可以呼叫到parent的class
+		self.volume = volume 
 
 d = Drink('vodka', 100, 600)
 print(d.volume)
