163f1999d4453e7ad79f91e8f3563571c939f1ef
lzlmorgan <morganliu0314@outlook.com>
2022-06-15 22:13:28 -0400

    1st dunder_method: __str__ & __repr__

---
+++ 進階觀念/dunder_method.py
@@ -0,0 +1,19 @@
+#__name__
+
+class Product:
+	def __init__(self, name, price):
+		self.name = name #property
+		self.price = price #property
+
+	#__str__決定如何print 簡易版
+	def __str__(self): 
+		#return self.name + ': $' + str(self.price)
+		return f'{self.name}: ${self.price}' #f string輸出
+
+	#representation 最完整輸出print
+	def __repr__(self): 
+		return f'<Product({self.name}, {self.price})>'
+
+p = Product('bublle tea', 60) #創建class類,然後建立class的實例instance
+print(p)
+print(repr(p))
