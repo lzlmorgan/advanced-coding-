ce26d79f80a6221a7f207caee43173742e2e102f
lzlmorgan <morganliu0314@outlook.com>
2022-06-17 18:11:19 -0400

    3rd version of adding def qqq()

--- 進階觀念/closure.py
+++ 進階觀念/closure.py
@@ -4,9 +4,13 @@
 
 def f():
 	x = 1 #enclose
+	def qqq():
+		print('asdasda')
+
 	def inner():
 		print(x) #local
-	return inner
+		qqq()
+	return inner #inner 直接封裝了def qqq
 
 y = f()
 print(y) #显示结果为<function f.<locals>.inner at 0x10276e710>
