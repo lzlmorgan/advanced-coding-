287c8a0d710ed7bac23005910676cecad22894c7
lzlmorgan <morganliu0314@outlook.com>
2022-06-17 18:01:35 -0400

    2nd version: return def inner and run y()

--- 進階觀念/closure.py
+++ 進階觀念/closure.py
@@ -1,10 +1,13 @@
 #closure 闭包
+#lifecycle
+
 
 def f():
-	x = 1
+	x = 1 #enclose
 	def inner():
-		print(x)
+		print(x) #local
 	return inner
 
 y = f()
 print(y) #显示结果为<function f.<locals>.inner at 0x10276e710>
+y()
