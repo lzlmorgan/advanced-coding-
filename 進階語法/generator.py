a2ba6af88d81e65f32434707e6ee2a7592eba330
lzlmorgan <morganliu0314@outlook.com>
2022-06-18 16:39:46 -0400

    1st version on list comprehension & yeild

---
+++ 進階語法/generator.py
@@ -0,0 +1,34 @@
+#generator  产生器iterator
+#通过yeild来实现 节约记忆体的存储位置
+
+def g():
+	while True:
+		yield 1
+
+print(next(g()))
+
+
+
+def my_range(n):
+	i = 0
+	while i < n:
+		print(f'start to print{i}')
+		yield i
+		i += 1
+
+for i in my_range(20):
+	print(i)
+
+print(my_range(5)) #生成iterator
+
+
+print('----------------------')
+#list comprehension
+x = [0, 1, 2, 3, 4]
+print([i*i for i in x]) #[]正常list
+print((i*i for i in x)) #()生成iterator
+it = (i*i for i in x)
+print(next(it))
+print(next(it))
+print(next(it)) #......
+
