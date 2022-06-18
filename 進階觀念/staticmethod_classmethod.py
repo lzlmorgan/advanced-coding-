fee43920255545ee033a149bf4de9cf1244b59a3
lzlmorgan <morganliu0314@outlook.com>
2022-06-18 00:21:05 -0400

    final version: static&class method

--- 進階觀念/staticmethod_classmethod.py
+++ 進階觀念/staticmethod_classmethod.py
@@ -1,4 +1,4 @@
-#staticmethod & classmethod 讓程序員不需要實例就可以使用
+#staticmethod & classmethod 不建議實例instance可以使用class的def
 
 class Batman:
 	def __init__(self, hp):
@@ -12,6 +12,14 @@ class Batman:
 	def calc_avg(x):
 		return sum(x) / len(x)
 
+	@classmethod	
+	def fffff(cls): #cls=classmethod cls彷彿建議一個instance接受數據
+		cls(hp).f() #instance hp=100
+
+#測試classmethod 需要運勢實例
+hp = 100
+Batman.fffff()
+print('------------------')
+#測試staticmethod 不需要建議實例
 x = [1, 2, 3,11]
 print(Batman.calc_avg(x))
-
